b672b7bb990ddf2a1b21ed2fe97ccf4e070e0511
Ahmed Alyafai <ahmedalyafai@gmail.com>
2025-01-24 19:11:01 +0800

    Changing directories

---
+++ Arrays/header/Arrays.hpp
@@ -0,0 +1,24 @@
+#ifndef Algorithms_hpp
+#define Algoirthms_hpp
+
+#include <iostream>
+using namespace std;
+class ArraysAlgo
+{
+private:
+    int [] array;
+    int size;
+    string indicator;
+
+public:
+    void InsertionSort(){}
+    void MergeSort(){}
+    void BubbleSort(){}
+    void QuickSort(){}
+};
+
+
+
+
+
+#endif
