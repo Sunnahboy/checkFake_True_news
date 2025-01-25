4594b41b6dda80d064f171f40586d2df17319aea
Abdulwahab <abdulwahabmoosa24@gmail.com>
2025-01-25 22:49:38 +0800

    testing main

--- Arrays/src/main.cpp
+++ Arrays/src/main.cpp
@@ -1,41 +1,61 @@
 #include <iostream>
 #include <limits>
+#include <fstream>
 #include "Interface.cpp"
 
 using namespace std;
 int main(){ 
-    Interface interface;
-    int choice;
-    interface.prinTable(4);
-    cout << "1. Data Filtering" << endl;
-    cout << "2. Sort Data" << endl;
-    cout << "3. Search Data" << endl;
-    cout << "4. FAQ" << endl;
-    cout << "5. End the Program" << endl;
-    interface.columns();
-    cout << "Enter Your choice: ";
-    while(!(cin >> choice)|| !(interface.RegInput(choice))){
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(),'\n');
-        cout << "Invaliad Input... Enter Your choice Again: ";
+//     Interface interface;
+//     int choice;
+//     interface.prinTable(4);
+//     cout << "1. Data Filtering" << endl;
+//     cout << "2. Sort Data" << endl;
+//     cout << "3. Search Data" << endl;
+//     cout << "4. FAQ" << endl;
+//     cout << "5. End the Program" << endl;
+//     interface.columns();
+//     cout << "Enter Your choice: ";
+//     while(!(cin >> choice)|| !(interface.RegInput(choice))){
+//         cin.clear();
+//         cin.ignore(numeric_limits<streamsize>::max(),'\n');
+//         cout << "Invaliad Input... Enter Your choice Again: ";
+//     }
+//     switch(choice){
+//         case 1: 
+//             interface.DataFilterting();
+//             break;
+//         case 2: 
+//             interface.SortData();
+//             break;
+//         case 3: 
+//             interface.SearchData();
+//             break;
+//         case 4: 
+//             interface.FAQ();
+//             break;
+//         case 5: 
+//             cout <<"Existing the program. GoodBye!";
+//             return 0;
+//         default: cout << "Invalid";
+//     }
+//     return 0;
+
+ifstream file("true.csv");
+
+	//step 3: insert the item to the linked list
+	string title, text, subject, date;
+	while (file.good()){
+
+		getline(file, title, ',');
+		getline(file, text, ',');
+		getline(file, subject, ',');
+		getline(file, date);
+
+        cout << date << endl;
     }
-    switch(choice){
-        case 1: 
-            interface.DataFilterting();
-            break;
-        case 2: 
-            interface.SortData();
-            break;
-        case 3: 
-            interface.SearchData();
-            break;
-        case 4: 
-            interface.FAQ();
-            break;
-        case 5: 
-            cout <<"Existing the program. GoodBye!";
-            return 0;
-        default: cout << "Invalid";
-    }
-    return 0;
+
+return 0;
 }
+
+
+
