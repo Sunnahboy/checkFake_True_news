767588b906d6be158c6de07e069a1452f7ae926e
Ahmed Alyafai <ahmedalyafai@gmail.com>
2025-01-25 21:31:08 +0800

    interface and main

--- Arrays/src/main.cpp
+++ Arrays/src/main.cpp
@@ -5,12 +5,37 @@
 using namespace std;
 int main(){ 
     Interface interface;
-    
-    string msg=interface.msg(4);
+    int choice;
+    interface.prinTable(4);
+    cout << "1. Data Filtering" << endl;
+    cout << "2. Sort Data" << endl;
+    cout << "3. Search Data" << endl;
+    cout << "4. FAQ" << endl;
+    cout << "5. End the Program" << endl;
     interface.columns();
-    cout <<string(26, ' ') << msg;
-    cout <<endl;
-    interface.columns();
-
+    cout << "Enter Your choice: ";
+    while(!(cin >> choice)|| !(interface.RegInput(choice))){
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(),'\n');
+        cout << "Invaliad Input... Enter Your choice Again: ";
+    }
+    switch(choice){
+        case 1: 
+            interface.DataFilterting();
+            break;
+        case 2: 
+            interface.SortData();
+            break;
+        case 3: 
+            interface.SearchData();
+            break;
+        case 4: 
+            interface.FAQ();
+            break;
+        case 5: 
+            cout <<"Existing the program. GoodBye!";
+            return 0;
+        default: cout << "Invalid";
+    }
     return 0;
 }
