1a5ec769a9a5fbd82d1586faaf62d8f788c9612c
Kashave Sathyvell <kashavesathyvell15@gmail.com>
2025-02-01 17:19:59 +0800

    Q2 using Array to search through instead of struct

--- Arrays/src/Statistics.cpp
+++ Arrays/src/Statistics.cpp
@@ -38,3 +38,38 @@ void calculateFakeNewsPercentage(NewsArticle* trueNews, int trueSize, NewsArticl
     cout << "Percentage of Fake Political News in 2016: " << fixed << setprecision(2) << percentage << "%" << endl;
 }
 
+
+
+void calculateFakeNewsPercentage(string** trueNews, int trueSize, string** fakeNews, int fakeSize) {
+    int truePolitical2016 = 0;
+    int fakePolitical2016 = 0;
+
+    // Loop through true news stored in 2D array
+    for (int i = 0; i < trueSize; i++) {
+        if (stoi(trueNews[i][3]) > 2016) break;  // Convert year from string to int
+        if (stoi(trueNews[i][3]) == 2016 && trueNews[i][2] == "politics") {
+            truePolitical2016++;
+        }
+    }
+
+    // Loop through fake news stored in 2D array
+    for (int i = 0; i < fakeSize; i++) {
+        if (stoi(fakeNews[i][3]) > 2016) break;  // Convert year from string to int
+        if (stoi(fakeNews[i][3]) == 2016 && fakeNews[i][2] == "politics") {
+            fakePolitical2016++;
+        }
+    }
+
+    int totalPolitical2016 = truePolitical2016 + fakePolitical2016;
+
+    if (totalPolitical2016 == 0) {
+        cout << "No political news articles found in 2016!" << endl;
+        return;
+    }
+
+    double percentage = (static_cast<double>(fakePolitical2016) / totalPolitical2016) * 100;
+
+    cout << "Total Political News Articles in 2016: " << totalPolitical2016 << endl;
+    cout << "Fake Political News Articles in 2016: " << fakePolitical2016 << endl;
+    cout << "Percentage of Fake Political News in 2016: " << fixed << setprecision(2) << percentage << "%" << endl;
+}
